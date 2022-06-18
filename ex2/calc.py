cca75255c4a7f48474a61547cb9f1d08a49d4922
takuma <c0a211201f@edu.teu.ac.jp>
2022-06-19 00:54:34 +0900

    3kaime

--- ex2/calc.py
+++ ex2/calc.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from  tkinter import messagebox as tkm
 
 calc=tk.Tk()
 calc.title("電卓")
@@ -7,8 +7,16 @@ calc.geometry("300x500")
 fonts=("Times New Roman",30)
 num_list=[[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2],[3,1]]
 count=9
+
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
+
+
 for i in num_list:
     button=tk.Button(calc,text=count,font=fonts,width=4,height=2)
     button.grid(row=i[0],column=i[1])
     count-=1
+    button.bind("<1>",button_click)
 calc.mainloop()
