438265de0dd6946b71cfd43180069114355387d4
takuma <c0a211201f@edu.teu.ac.jp>
2022-06-19 00:36:27 +0900

    2kaime

--- ex2/calc.py
+++ ex2/calc.py
@@ -1,5 +1,14 @@
 import tkinter as tk
 
+
 calc=tk.Tk()
+calc.title("電卓")
 calc.geometry("300x500") 
+fonts=("Times New Roman",30)
+num_list=[[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2],[3,1]]
+count=9
+for i in num_list:
+    button=tk.Button(calc,text=count,font=fonts,width=4,height=2)
+    button.grid(row=i[0],column=i[1])
+    count-=1
 calc.mainloop()
